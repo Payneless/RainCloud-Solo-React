2052b68f01cd77132bc14c0f989360699ee3d39f
Jake Payne <paynekid96@gmail.com>
2021-11-04 10:53:16 -0700

    phase 4 complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -28,6 +28,14 @@ router.post(
     });
   })
 );
+router.get("/", restoreUser, (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      user: user.toSafeObject(),
+    });
+  } else return res.json({});
+});
 
 router.delete("/", (_req, res) => {
   res.clearCookie("token");
