183c81e28d8466da03bb44cffd0cbd689b1cdc01
Jake Payne <paynekid96@gmail.com>
2021-11-04 13:26:15 -0700

    routes finished

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import ReactDOM from "react-dom";
 
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { restoreCSRF, csrfFetch } from "./store/csrf.js";
+import App from "./App";
 
 import configureStore from "./store";
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
