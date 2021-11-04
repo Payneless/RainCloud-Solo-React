d2f7083d744030a51618bf60a8e08572eb75c7c8
Jake Payne <paynekid96@gmail.com>
2021-11-04 10:30:41 -0700

    login route added

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
-const asyncHandler = require("express-async-handler");
-const { setTokenCookie } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
-const { restoreUser } = require("../../utils/auth.js");
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+router.post("/test", (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 // router.get("/restore-user", restoreUser, (req, res) => {
 //   return res.json(req.user);
