21d1fc390be993a2820909da5839bfb5e210c23e
Jake Payne <paynekid96@gmail.com>
2021-11-05 11:12:54 -0700

    profile button and logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import LoginFormPage from "./components/LoginFormPage";
 import { useDispatch } from "react-redux";
 import * as sessionActions from "./store/session";
 import SignupFormPage from "./components/SignUpFormPage";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,15 +15,19 @@ function App() {
 
   return (
     <div>
-      <nav className="nav-bar">Rain Cloud</nav>
-      <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
+      <nav className="nav-bar">
+        <Navigation isLoaded={isLoaded} />
+        {isLoaded && (
+          <Switch>
+            <Route path="/login">
+              <LoginFormPage />
+            </Route>
+            <Route path="/signup">
+              <SignupFormPage />
+            </Route>
+          </Switch>
+        )}
+      </nav>
     </div>
   );
 }
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,27 +1,29 @@
 import { NavLink } from "react-router-dom";
+import ProfileButton from "./ProfileButton";
+import { useSelector } from "react-redux";
+import "./Navigation.css";
 
 const Navigation = ({ isLoaded }) => {
   const sessionUser = useSelector((state) => state.session.user);
 
-	if(sessionUser) {
-		sessionLinks = (
-
-		)
-	} else {
-		sessionLinks = (
-			<>
-				<NavLink to="/login"> Log In</NavLink>
-				<NavLink to="/signup">Sign Up</NavLink>
-			</>
-		);
-	}
+  let sessionButtons;
+  if (sessionUser) {
+    sessionButtons = <ProfileButton user={sessionUser} />;
+  } else {
+    sessionButtons = (
+      <>
+        <NavLink to="/login"> Log In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </>
+    );
+  }
   return (
     <ul>
       <li>
         <NavLink exact to="/">
           Rain Cloud
         </NavLink>
-				{isLoaded && sessionLinks}
+        {isLoaded && sessionButtons}
       </li>
     </ul>
   );
