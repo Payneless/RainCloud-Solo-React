17cdf0b5da92d02cf563f38f60efad3d721d5edd
Jake Payne <paynekid96@gmail.com>
2021-11-09 14:41:33 -0700

    added sound post route

--- backend/routes/api/sounds.js
+++ backend/routes/api/sounds.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
-const { check } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 const { User, Sound } = require("../../db/models");
 
 const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
+
+const validateSound = [
+  check("name")
+    .not()
+    .isEmpty()
+    .withMessage("Please provide a name for your sound."),
+  check("content")
+    .not()
+    .isEmpty()
+    .withMessage("Please provide a description for your sound."),
+  check("file")
+    .isURL()
+    .withMessage("Please provide a url where your file is uploaded."),
+];
 
 router.get(
   "/",
@@ -16,11 +30,24 @@ router.get(
   })
 );
 
-// router.post(
-//   "/",
-//   asyncHandler(async (req, res) => {
-//     const newSound = await Sound.create();
-//   })
-// );
+router.post(
+  "/",
+  validateSound,
+  asyncHandler(async (req, res) => {
+    const { name, content, file, userId, playlistId } = req.body;
+    const soundErrors = validationResult(req);
+
+    if (soundErrors.isEmpty()) {
+      const newSound = await Sound.create({
+        name,
+        content,
+        userId,
+        playlistId,
+        file,
+      });
+    }
+    return res.json({ newSound });
+  })
+);
 
 module.exports = router;
